40eab8daed8f1b2f76879eb3484c41af5c13773b
Brahim Nedjar <skullneed@gmail.com>
2023-09-04 20:58:15 +0100

    first commit

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+
+import '../db/db_helper.dart';
+import '../models/tasks.dart';
+
+class TaskController extends GetxController{
+  @override
+  void onReady(){
+    getTasks();
+    super.onReady();
+  }
+
+  var taskList = <Task>[].obs;
+
+  Future<int> addTask({Task? task}) async {
+    return await DBHelper.insert(task);
+  }
+
+  void getTasks() async{
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
+    print('tasks are here');
+  }
+
+  void delete(Task task){
+    DBHelper.delete(task);
+  }
+}
---
+++ lib/widget/input_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../ui/theme.dart';
+
+class MyInputField extends StatelessWidget {
+  final String title;
+  final String hint;
+  final TextEditingController? controller;
+  final Widget? widget;
+  const MyInputField({Key? key, required this.title, required this.hint, this.widget, this.controller,  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: titleStyle,
+          ),
+          Container(
+            height: 46,
+            margin: EdgeInsets.only(top: 8),
+            padding: EdgeInsets.only(left: 14),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                      readOnly: widget==null?false:true,
+                      autofocus: false,
+                      cursorColor: Get.isDarkMode? Colors.grey[100] : Colors.grey[700],
+                      // controller: controller,
+                      style: subTitleStyle,
+                      decoration: InputDecoration(
+                        hintText: hint,
+                        hintStyle: subTitleStyle,
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: context.theme.backgroundColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                ),
+                widget==null?Container():Container(child: widget,),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
